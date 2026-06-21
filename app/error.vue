8af9ce38658b1e89c3a53850c4d666ed5e272d3a
Kanjanawat <m.kanjanawat@gmail.com>
2026-06-21 15:15:53 +0700

    [Add] Handle not existing URL with error page

---
+++ app/error.vue
@@ -0,0 +1,26 @@
+<template>
+    <Navbar />
+    <div class="flex min-h-screen justify-center items-center bg-gray-100">
+        <div class="flex flex-col text-center gap-3">
+            <div class="animate-bounce">
+                <span class=" text-6xl">😭</span>
+                <h1 class="text-2xl font-bold">404</h1>
+                <p class=" font-light">Not found the page</p>
+            </div>
+            <nuxt-link to="/" class=" hover:underline">Back to Home page 🏠</nuxt-link>
+        </div>
+    </div>
+    <Footer />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    error: Object
+})
+
+const handleClearError = () => {
+    clearError({ redirect: '/' })
+}
+</script>
+
+<style scoped></style>
